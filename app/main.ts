d3d5047cd8f9fa180f05803464dd1902f4212fc4
Ayush <codesbyayush.gmail.com>
2024-04-04 15:14:23 +0530

    pass 2nd stage

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+  connection.on("data", (data: Buffer) => {
+    const req = data.toString().trim();
+    if (req === "*1\r\n$4\r\nping\r\n") {
+      connection.write("+PONG\r\n");
+    }
+    connection.end();
+  });
 });
 
 server.listen(6379, "127.0.0.1");
