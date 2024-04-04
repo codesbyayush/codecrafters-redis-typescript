537ea8b1d0f01801966f0da86735741e0e16b151
Ayush <codesbyayush.gmail.com>
2024-04-04 17:59:22 +0530

    pass 5th stage

--- app/main.ts
+++ app/main.ts
@@ -12,7 +12,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     const parsedReq = RESP2parser(req.split("\r\n"));
     if (req.includes("ping")) connection.write("+PONG\r\n");
     else if (parsedReq[0] === "echo")
-      parsedReq.map((reqs) => {
+      parsedReq.forEach((reqs) => {
         if (reqs !== "echo") connection.write(reqs);
       });
     // connection.end();
