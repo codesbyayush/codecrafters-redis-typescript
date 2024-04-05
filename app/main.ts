1a75ff1af29c151ccb04e70bb7e27f00e2e99f1f
Ayush <codesbyayush.gmail.com>
2024-04-05 14:37:48 +0530

    replication - 04

--- app/main.ts
+++ app/main.ts
@@ -110,8 +110,8 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     }
 
     if (parsedReq[0].toLowerCase() === "set") {
-      forwardToReplicas(data);
       console.log(map);
+      forwardToReplicas(data);
       map[parsedReq[1]] = parsedReq[2];
       if (parsedReq.length > 3 && parsedReq[3] === "px") {
         let expTime = Number(Date.now());
