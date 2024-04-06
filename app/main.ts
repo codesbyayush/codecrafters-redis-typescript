05828adf1dd7e055af99dfcb8f189a6dd3b59719
Ayush <codesbyayush.gmail.com>
2024-04-06 11:01:52 +0530

    replication - 04

--- app/main.ts
+++ app/main.ts
@@ -142,7 +142,8 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     }
 
     if (parsedReq.includes("wait")) {
-      await waitFn(Number(parsedReq[parsedReq.indexOf("wait") + 2]));
+      if (Number(parsedReq[parsedReq.indexOf("wait") + 1]) > 0)
+        await waitFn(Number(parsedReq[parsedReq.indexOf("wait") + 2]));
       connection.write(`:${replicas.length}\r\n`);
     }
 
